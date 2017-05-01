aab79a2c205947972c0b07cf305d26859a9b21a4
Antoine Baché <antoine.bache@epitech.eu>
2017-05-01 02:53:50 +0200

    Added memory lib

---
+++ libs/memory/include/memory_stdafx.hpp
@@ -0,0 +1,18 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#ifndef MEMORY_STDAFX_HPP_
+#define MEMORY_STDAFX_HPP_
+
+#include "memory_targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+// Reference additional headers your program requires here
+
+#include "MemoryManager.hpp"
+#include "AllocableObject.hpp"
+
+#endif // !MEMORY_STDAFX_HPP_
