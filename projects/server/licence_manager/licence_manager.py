61db13bbe66e66f2e1de024483e9be3829bd13dd
Antoine Baché <antoine.bache@epitech.eu>
2017-05-03 00:43:43 +0200

    License manager finished

--- projects/server/licence_manager/licence_manager.py
+++ projects/server/licence_manager/licence_manager.py
@@ -6,8 +6,11 @@ It allows someone to add licences, or list all the current licences.
 """
 import logging
 import random
+import socket
+import select
 
 LICENCE_FILE = "./.licence_keys"
+SERVER_PORT = 1123
 
 
 def generate_licence():
@@ -31,7 +34,7 @@ def find_licence(key):
     return rc != -1
 
 
-def add_licence():
+def add_licence(clientsocket):
     # Add a licence key to the licence file
     key = generate_licence()
 
@@ -42,6 +45,13 @@ def add_licence():
     print(key)
     with open(LICENCE_FILE, 'a') as f:
         f.write(key + '\n')
+    totalsent = 0
+    msg = "License key file changed"
+    while totalsent < len(msg):
+        sent = clientsocket.send(msg[totalsent:].encode())
+        if sent == 0:
+            raise RuntimeError("Socket connection broken")
+        totalsent = totalsent + sent
 
 
 def list_licence():
@@ -58,24 +68,56 @@ logging.basicConfig(format='%(asctime)s %(message)s',
 
 logging.info('Starting licence manager.')
 logging.info('Writing to file ' + LICENCE_FILE)
+
+# Create socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serversocket.bind(('localhost', SERVER_PORT))
+serversocket.listen(1)
+logging.info('Local server listening on port ' + str(SERVER_PORT))
+
+
+# Waiting for ConnectManager connection
+(clientsocket, address) = serversocket.accept()
+logging.info('Client connected !')
+
 # Licence manager loop
-while True:
-    print('> ', end='')
+try:
+    while True:
+        # Process user input
+        print('> ', end='')
 
-    # Get input
-    msg = input()
-    logging.debug('User typed: ' + msg)
+        # Get input
+        msg = input()
+        logging.debug('User typed: ' + msg)
 
-    # Process input
-    if msg == 'exit':
-        break
-    elif msg == 'help':
-        print('Available commands: exit / help / list / add')
-    elif msg == 'list':
-        list_licence()
-    elif msg == 'add':
-        add_licence()
-    else:
-        print('Invalid command')
+        try:
+            ready_to_read, ready_to_write, in_error = select.select(
+                [clientsocket], [clientsocket, ], [], 5)
+        except select.error:
+            # 0 = done receiving, 1 = done sending, 2 = both
+            clientsocket.shutdown(2)
+            clientsocket.close()
+            # connection error event here
+            print('Connection error')
+            break
+
+        # If there's data to read, disconnect
+        if len(ready_to_read) > 0:
+            break
+
+        # Process input
+        if msg == 'exit':
+            break
+        elif msg == 'help':
+            print('Available commands: exit / help / list / add')
+        elif msg == 'list':
+            list_licence()
+        elif msg == 'add':
+            add_licence(clientsocket)
+        else:
+            print('Invalid command')
+except Exception as e:
+    print(e)
 
 logging.info('Stopping licence manager.')
