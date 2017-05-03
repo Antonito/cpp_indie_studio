9808ecf256792d7c460452f0008a82454602b4f3
Antoine Baché <antoine.bache@epitech.eu>
2017-05-03 11:12:05 +0200

    Working on connect manager

--- projects/server/connect_manager/src/main.cpp
+++ projects/server/connect_manager/src/main.cpp
@@ -2,5 +2,16 @@
 
 int main()
 {
+	std::cout << "Starting server" << std::endl;
+
+	// Connection to License Manager
+	// Accept Game Server's connection
+
+	// Accept Client's connection
+
+	std::cout << "Server stopped" << std::endl;
+#if defined(_WIN32) && !defined(__on__linux__)
+	system("pause");
+#endif
   return (0);
 }
