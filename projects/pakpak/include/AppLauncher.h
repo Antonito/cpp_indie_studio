ffe2a429e4f8c01279632a1d94c79e0127f1eba1
ludonope <ludovic.petrenko@outlook.com>
2017-05-05 18:05:49 +0200

    Added application

---
+++ projects/pakpak/include/AppLauncher.h
@@ -0,0 +1,29 @@
+#ifndef APPLAUNCHER_H_
+#define APPLAUNCHER_H_
+
+#include <memory>
+#include <Ogre.h>
+#include "InputListener.h"
+
+class AppLauncher
+
+{
+
+public:
+  AppLauncher();
+
+  ~AppLauncher();
+
+  bool start();
+
+  void createFrameListener();
+
+private:
+  std::unique_ptr<Ogre::Root>         mRoot;
+  std::unique_ptr<Ogre::RenderWindow> mWindow;
+  Ogre::SceneManager *                mSceneMgr;
+  std::unique_ptr<Ogre::Camera>       mCamera;
+  std::unique_ptr<InputListener>      mInputListener;
+};
+
+#endif // !APPLAUNCHER_H_
---
+++ projects/pakpak/src/InputListener.cpp
@@ -0,0 +1,83 @@
+#include "InputListener.h"
+
+InputListener::InputListener(Ogre::RenderWindow *wnd, Ogre::Camera *camera)
+    : mWindow(wnd), mCamera(camera)
+{
+  startOIS();
+}
+
+InputListener::~InputListener()
+{
+  Ogre::WindowEventUtilities::removeWindowEventListener(mWindow, this);
+  windowClosed(mWindow);
+}
+
+bool InputListener::frameRenderingQueued(const Ogre::FrameEvent &evt)
+{
+  if (mWindow->isClosed())
+    return false;
+
+  mKeyboard->capture();
+  mMouse->capture();
+
+  if (mKeyboard->isKeyDown(OIS::KC_ESCAPE))
+    return false;
+
+  return true;
+}
+
+void InputListener::startOIS()
+{
+  Ogre::LogManager::getSingletonPtr()->logMessage("*** Initializing OIS ***");
+  // Change logger ?
+
+  // Get the identifier of the RenderWindow
+  OIS::ParamList     pl;
+  size_t             windowHnd = 0;
+  std::ostringstream windowHndStr;
+
+  mWindow->getCustomAttribute("WINDOW", &windowHnd);
+  windowHndStr << windowHnd;
+  pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
+
+  // Create Input System
+  mInputManager = OIS::InputManager::createInputSystem(pl);
+
+  mMouse = static_cast<OIS::Mouse *>(
+      mInputManager->createInputObject(OIS::OISMouse, false));
+
+  mKeyboard = static_cast<OIS::Keyboard *>(
+      mInputManager->createInputObject(OIS::OISKeyboard, false));
+
+  // Set the auto Resize
+  windowResized(mWindow);
+  Ogre::WindowEventUtilities::addWindowEventListener(mWindow, this);
+}
+
+void InputListener::windowResized(Ogre::RenderWindow *wnd)
+{
+  // Get the resized window metrics
+  unsigned int width, height, depth;
+  int          left, top;
+
+  wnd->getMetrics(width, height, depth, left, top);
+  const OIS::MouseState &ms = mMouse->getMouseState();
+  ms.width = width;
+  ms.height = height;
+}
+
+void InputListener::windowClosed(Ogre::RenderWindow *wnd)
+{
+  // Delete all Object when the window close
+  if (wnd == mWindow)
+    {
+      if (mInputManager)
+	{
+	  mInputManager->destroyInputObject(mMouse);
+	  mInputManager->destroyInputObject(mKeyboard);
+
+	  OIS::InputManager::destroyInputSystem(mInputManager);
+	  mInputManager = 0;
+	}
+    }
+}
