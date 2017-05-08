3a0b20d13e5263b733f2afc04f5de1289043c628
marc brout <marc.brout@epitech.eu>
2017-05-08 13:50:49 +0200

    Pushing old solution

---
+++ libs/pythonpp/include/PythonModule.hpp
@@ -0,0 +1,97 @@
+//
+// Created by brout_m on 03/05/17.
+//
+
+#ifndef CPP_INDIE_STUDIO_PYMODULE_HPP
+#define CPP_INDIE_STUDIO_PYMODULE_HPP
+
+#include <python2.7/Python.h>
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <typeindex>
+#include "PyFunctionReturnError.hpp"
+#include "PyFunctionArgError.hpp"
+#include "Logger.hpp"
+#include "PythonLiteralConverter.hpp"
+#include "apply.hpp"
+
+namespace pythonpp
+{
+  class PythonFunction
+  {
+  public:
+    explicit PythonFunction(PyObject *module, std::string const &functionName);
+    ~PythonFunction();
+
+    template <typename Ret, typename... Args>
+    Ret exec(Args... args)
+    {
+      const std::int32_t  n {sizeof...(args)};
+      std::tuple<Args...> tuple(args...);
+      PyObject *          retValue {nullptr};
+      PyObject *          pyArgs {nullptr};
+      std::int32_t        index {0};
+      Ret                 convertedRet;
+
+      pyArgs = PyTuple_New(n);
+
+      nope::serialization::apply<bool, Args...>(tuple, [this, &index, &pyArgs](auto &e)->bool
+      {
+        if (!PythonLiteralConverter::pushArgs<decltype(e)>(pyArgs, e, index))
+          throw(pythonpp::PyFunctionArgError(this->m_name));
+        return (true);
+      });
+
+      retValue = PyObject_CallObject(m_function, pyArgs);
+      Py_DECREF(pyArgs);
+      if (!retValue)
+      {
+        throw(pythonpp::PyFunctionReturnError(m_name));
+      }
+      convertedRet =
+          std::move(PythonLiteralConverter::backConverter<Ret>(retValue));
+      Py_DECREF(retValue);
+      return (convertedRet);
+    }
+
+  private:
+    PyObject *  m_function;
+    std::string m_name;
+
+    PythonFunction(PythonFunction const &) = delete;
+    PythonFunction operator=(PythonFunction const &) = delete;
+  };
+
+  class PythonModule
+  {
+  public:
+    explicit PythonModule(std::string const &moduleName);
+    ~PythonModule();
+    PyObject *ptr();
+    void feedFunctions(std::vector<std::string> const &functionNames);
+
+    template <typename Ret, typename... Args>
+    bool exec(std::size_t const index, Ret &out, Args... args)
+    {
+      try
+      {
+        out = m_functions[index]->exec<Ret, Args...>(args...);
+      }
+      catch (indie::AException &err)
+      {
+        Log(nope::log::Warning) << err.what() << std::endl;
+        return (false);
+      }
+      return (true);
+    }
+
+  private:
+    PyObject *                                   m_module;
+    std::vector<std::unique_ptr<PythonFunction>> m_functions;
+    PythonModule(PythonModule const &) = delete;
+    PythonModule &operator=(PythonModule const &) = delete;
+  };
+}
+#endif // CPP_INDIE_STUDIO_PYMODULE_HPP
---
+++ libs/pythonpp/src/PythonModule.cpp
@@ -0,0 +1,77 @@
+//
+// Created by brout_m on 03/05/17.
+//
+
+#include <PyFunctionInitializationError.hpp>
+#include "PythonModule.hpp"
+#include "PyInitializationError.hpp"
+#include "PyFunctionInitializationError.hpp"
+#include "Logger.hpp"
+
+pythonpp::PythonModule::~PythonModule()
+{
+  Py_DECREF(m_module);
+}
+
+pythonpp::PythonModule::PythonModule(std::string const &moduleName)
+    : m_module(nullptr), m_functions()
+{
+  PyObject *name;
+
+  Py_Initialize();
+  if (!Py_IsInitialized())
+    throw(pythonpp::PyInitializationError(
+        "Error while Initializating module " + moduleName));
+
+  name = PyString_FromString(moduleName.c_str());
+  if (!name)
+    throw(pythonpp::PyInitializationError("Module '" + moduleName +
+                                          "' doesn't exist."));
+  Py_DecRef(name);
+
+  m_module = PyImport_Import(name);
+  if (!m_module)
+    throw(pythonpp::PyInitializationError("Failed to load '" + moduleName +
+                                          "'"));
+}
+
+PyObject *pythonpp::PythonModule::ptr()
+{
+  return m_module;
+}
+
+void pythonpp::PythonModule::feedFunctions(
+    std::vector<std::string> const &functionNames)
+{
+  std::vector<std::string> stolenNames = std::move(functionNames);
+
+  for (std::string const &name : stolenNames)
+    {
+      try
+      {
+        m_functions.push_back(std::make_unique<PythonFunction>(m_module, name));
+      }
+      catch (pythonpp::PyFunctionInitializationError &err)
+      {
+        Log(nope::log::Warning) << err.what() << std::endl;
+      }
+    }
+}
+
+pythonpp::PythonFunction::~PythonFunction()
+{
+  Py_XDECREF(m_function);
+}
+
+pythonpp::PythonFunction::PythonFunction(
+    PyObject *module, std::string const &functionName)
+    : m_function(nullptr), m_name(functionName)
+{
+  m_function = PyObject_GetAttrString(module, functionName.c_str());
+  if (!m_function)
+    throw(pythonpp::PyFunctionInitializationError("Function '" + functionName +
+                                                  "' doesn't exist"));
+  if (!PyCallable_Check(m_function))
+    throw(pythonpp::PyFunctionInitializationError("Object '" + functionName +
+                                                  "' is not callable"));
+}
