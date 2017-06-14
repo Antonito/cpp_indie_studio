51e40bace5a17a298781e40a174e2e072f9a7451
benjamin.duhieu <benjamin.duhieu@epitech.eu>
2017-06-14 09:29:33 +0200

    Ckang format

--- projects/pakpak/src/ia/ia.cpp
+++ projects/pakpak/src/ia/ia.cpp
@@ -6,44 +6,42 @@
 
 Ia::~Ia()
 {
-
 }
 
 Ia::Ia(Ogre::Vector3 &l_posCar) : m_pos(l_posCar), m_curNode(0)
 {
-
 }
 
 void Ia::feedNodeRace(std::vector<Ogre::Vector3> &l_nodes)
 {
-    m_nodes = l_nodes;
+  m_nodes = l_nodes;
 }
 
 Ogre::Vector3 &Ia::getPos() const
 {
-    return m_pos;
+  return m_pos;
 }
 
 void Ia::race(Ogre::Vector3 &l_carPos, Ogre::Vector3 &l_carDir, float l_speed)
 {
-    m_pos = l_carPos;
-    distNode();
-    l_carDir = m_nodes[m_curNode] - m_pos;
+  m_pos = l_carPos;
+  distNode();
+  l_carDir = m_nodes[m_curNode] - m_pos;
 }
 
 void Ia::distNode()
 {
-    Ogre::Vector3 m_relativeVector = m_nodes[m_curNode] - m_pos;
-    if (m_relativeVector.length() < 0.5)
+  Ogre::Vector3 m_relativeVector = m_nodes[m_curNode] - m_pos;
+  if (m_relativeVector.length() < 0.5)
     {
-        if (m_curNode == m_nodes.size() - 1)
-            m_curNode = 0;
-        else
-            m_curNode++;
+      if (m_curNode == m_nodes.size() - 1)
+	m_curNode = 0;
+      else
+	m_curNode++;
     }
 }
 
 Ogre::Vector3 &Ia::getDir() const
 {
-    return m_newDir;
+  return m_newDir;
 }
--- projects/pakpak/src/ia/ia.hpp
+++ projects/pakpak/src/ia/ia.hpp
@@ -17,16 +17,17 @@ public:
   Ia(Ogre::Vector3 &);
   virtual ~Ia();
 
-  void                  feedNodeRace(std::vector<Ogre::Vector3> &);
-  Ogre::Vector3         &getPos() const;
-    void                race(Ogre::Vector3 &, Ogre::Vector3 &, float);
-    void                distNode();
-    Ogre::Vector3       &getDir() const;
+  void           feedNodeRace(std::vector<Ogre::Vector3> &);
+  Ogre::Vector3 &getPos() const;
+  void           race(Ogre::Vector3 &, Ogre::Vector3 &, float);
+  void           distNode();
+  Ogre::Vector3 &getDir() const;
+
 private:
   Ogre::Vector3              m_pos;
   std::int32_t               m_curNode;
   std::vector<Ogre::Vector3> m_nodes;
-    Ogre::Vector3            m_newDir;
+  Ogre::Vector3              m_newDir;
 };
 
 #endif // CPP_INDIE_STUDIO_IA_HPP
