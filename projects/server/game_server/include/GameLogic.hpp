6f64bb836734d94e08107a980c1f6fe0639e7740
Antoine Baché <antoine.bache@epitech.eu>
2017-06-14 10:16:43 +0200

    Working on gameLogic

--- projects/server/game_server/include/GameLogic.hpp
+++ projects/server/game_server/include/GameLogic.hpp
@@ -2,6 +2,7 @@
 #define GAME_LOGIC_HPP_
 
 #include <cstddef>
+#include "ConfigTools.hpp"
 
 // Disable clang warning for implicit padding
 #if defined(__clang__)
@@ -11,6 +12,9 @@
 
 class GameLogic
 {
+  // TODO: Currently with MapConfig, should be something else in the future
+  using map_t = MapConfig;
+
 public:
   GameLogic();
   GameLogic(GameLogic const &);
@@ -22,8 +26,7 @@ public:
   ~GameLogic();
 
   // General configuration
-  void setUp();
-  void run();
+  void run(); // Thread
 
   // Stop current game, and play the next scheduled map
   void nextMap();
@@ -34,13 +37,43 @@ public:
   std::size_t getCurrentAIs() const;
   std::size_t getCurrentSpectator() const;
 
+  // Maps
+  map_t const &getCurrentMap() const;
+
   void addHumanPlayer();
 
 private:
+  enum class GameState : std::uint16_t
+  {
+    LOADING,
+    PLAYING
+  };
+
+  void setUp();
+
   void addSpectator();
   void addAI();
+  void deleteAI();
+
+  void loadMap();
+
+  void setState(GameState const state);
+
+  void loadingState();
+  void playingState();
+
+  // General configuration
+  std::atomic<bool> m_running;
+  std::size_t       m_maxPlayers;
+  std::size_t       m_currentHumanPlayers;
+  std::size_t       m_currentAIs;
+  std::size_t       m_currentSpectators;
 
   // TODO: GameData ?
+  std::vector<map_t>           m_maps;
+  std::vector<map_t>::iterator m_currentMap;
+  GameState                    m_prevState;
+  GameState                    m_state;
 };
 
 // Disable clang warning for implicit padding
--- projects/server/game_server/src/GameLogic.cpp
+++ projects/server/game_server/src/GameLogic.cpp
@@ -1,6 +1,9 @@
 #include "game_server_stdafx.hpp"
 
 GameLogic::GameLogic()
+    : m_running(false), m_maxPlayers(), m_currentHumanPlayers(),
+      m_currentAIs(), m_currentSpectators(), m_maps(), m_currentMap(),
+      m_prevState(GameState::LOADING), m_state(GameState::LOADING)
 {
 }
 
@@ -22,48 +25,135 @@ GameLogic::~GameLogic()
 
 void GameLogic::setUp()
 {
-  nope::log::Log(Info) << "Setting Up GameLogic";
   // TODO
+  // Get config, load maps
+  nope::log::Log(Info) << "Setting Up GameLogic";
+
+  m_maxPlayers = static_cast<std::size_t>(
+      Config::getInstance().getGameServerMaxClients());
+  std::vector<MapConfig> const &mapConfig =
+      Config::getInstance().getMapConfig();
+  if (mapConfig.size() < 1)
+    {
+      nope::log::Log(Error) << "Incorrect number of maps [GameLogic]";
+      throw std::exception(); // TODO
+    }
+  m_maps = mapConfig;
+  m_currentMap = m_maps.begin();
+  m_running = true;
+}
+
+void GameLogic::loadingState()
+{
+  assert(m_state == GameState::LOADING);
+  if (m_prevState == GameState::PLAYING)
+    {
+      // Stop previous game
+    }
+
+  // Check if any human players (players or spectators)
+  if (getCurrentPlayers() > 0)
+    {
+      // Loop over clients
+      // Wait for all of their signal
+    }
+  setState(GameState::PLAYING);
+}
+
+void GameLogic::playingState()
+{
+  assert(m_state == GameState::PLAYING);
+  // Get packets
+  // GameLogic process
+  // Send packets
 }
 
 void GameLogic::run()
 {
+  // Configure game logic
+  setUp();
   nope::log::Log(Info) << "GameLogic started";
-  // TODO
+
+  // Loop
+  while (m_running)
+    {
+      if (m_state == GameState::LOADING)
+	{
+	  loadingState();
+	}
+      else
+	{
+	  playingState();
+	}
+    }
+}
+
+void GameLogic::nextMap()
+{
+  // Play next map
+  nope::log::Log(Debug) << "Changing map";
+  ++m_currentMap;
+  if (m_currentMap == m_maps.end())
+    {
+      m_currentMap = m_maps.begin();
+    }
+  setState(GameState::LOADING);
 }
 
 // GamePlayer infos
 std::size_t GameLogic::getCurrentPlayers() const
 {
-  return (getCurrentHumanPlayers() + getCurrentAIs());
+  return (getCurrentHumanPlayers() + getCurrentSpectator());
 }
 
 std::size_t GameLogic::getCurrentHumanPlayers() const
 {
-  // TODO
-  return (0);
+  return (m_currentHumanPlayers);
 }
 
 std::size_t GameLogic::getCurrentAIs() const
 {
-  // TODO
-  return (0);
+  return (m_currentAIs);
 }
 
 std::size_t GameLogic::getCurrentSpectator() const
 {
-  // TODO
-  return (0);
+  return (m_currentSpectators);
 }
 
+GameLogic::map_t const &GameLogic::getCurrentMap() const
+{
+  return (*m_currentMap);
+}
+
+// Players
 void GameLogic::addHumanPlayer()
 {
+  // TODO
 }
 
 void GameLogic::addAI()
 {
+  // TODO
+}
+
+void GameLogic::deleteAI()
+{
+  // TODO
 }
 
 void GameLogic::addSpectator()
 {
+  // TODO
+}
+
+void GameLogic::loadMap()
+{
+  // TODO
+}
+
+void GameLogic::setState(GameState const state)
+{
+  m_prevState = m_state;
+  m_state = state;
 }
