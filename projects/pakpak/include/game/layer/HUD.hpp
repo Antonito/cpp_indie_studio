c8597f41ecbc8b00d5721eab78c749d815a34e30
marc brout <marc.brout@epitech.eu>
2017-06-14 13:03:22 +0200

    hud exploration

--- projects/pakpak/include/game/layer/HUD.hpp
+++ projects/pakpak/include/game/layer/HUD.hpp
@@ -5,6 +5,9 @@
 #ifndef CPP_INDIE_STUDIO_HUD_HPP
 #define CPP_INDIE_STUDIO_HUD_HPP
 
+#include <CEGUI/RendererModules/Ogre/Renderer.h>
+#include <OIS/OISPrereqs.h>
+
 namespace core
 {
   class HUD
@@ -12,6 +15,26 @@ namespace core
   public:
     HUD();
     ~HUD();
+    HUD &operator=(HUD const &) = delete;
+    HUD &operator=(HUD &&) = delete;
+    HUD(HUD const &) = delete;
+    HUD(HUD &&) = delete;
+    void           initHUD();
+    void           destroyHUD();
+    void           loadScheme(const std::string &);
+    void           loadLayout(const std::string &);
+    void           addLayout(const std::string &);
+    void           removeLayout(const std::string &);
+    void           grabMouse();
+    void           showCursor(bool);
+    void           hideCursor(bool);
+    CEGUI::Window &getChild(const std::string &);
+
+  private:
+    CEGUI::OgreRenderer *m_renderer;
+    CEGUI::Window *      m_rootWindow;
+    CEGUI::GUIContext *  m_context;
+    OIS::ParamList       m_param;
   };
 }
 
--- projects/pakpak/src/game/layer/HUD.cpp
+++ projects/pakpak/src/game/layer/HUD.cpp
@@ -4,3 +4,47 @@
 
 #include "HUD.hpp"
 
+core::HUD::HUD()
+    : m_renderer(nullptr), m_rootWindow(nullptr), m_context(nullptr), m_param()
+{
+}
+
+core::HUD::~HUD()
+{
+}
+
+void core::HUD::initHUD()
+{
+}
+
+void core::HUD::destroyHUD()
+{
+}
+
+void core::HUD::loadScheme(const std::string &)
+{
+}
+
+void core::HUD::loadLayout(const std::string &path)
+{
+  if (m_rootWindow)
+    destroyHUD();
+  m_rootWindow = CEGUI::WindowManager::get
+}
+
+void core::HUD::addLayout(const std::string &)
+{
+}
+
+void core::HUD::removeLayout(const std::string &)
+{
+}
+
+CEGUI::Window &core::HUD::getChild(const std::string &)
+{
+  return <#initializer #>;
+}
+
+void core::HUD::grabMouse()
+{
+}
